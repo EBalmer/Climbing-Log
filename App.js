9b5e370e43696786e1631c23480b25f62b61d784
Edward Balmer <edwardbalmer@gmail.com>
2020-01-31 17:12:16 +0000

    Added session card component

--- App.js
+++ App.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SessionLog from './components/sessionLog';
 
+
 export default function App() {
+
+
+  // fetch('http://192.168.1.97:3000/users', {method: "GET"})
+  // .then((response) => {
+  //   return response.json()
+  // })
+  // .then((myJson) => {
+  //   console.log(myJson)
+  // }
+  // )
+  // .catch((error) => {
+  //   console.log(error)}
+  // );
+
   return (
     <SessionLog />
   );
---
+++ components/sessionCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+export default class SessionCard extends Component {
+
+    render() {
+        return (
+            <Card>
+                <View>
+                    <Text>{this.props.date}</Text>
+                    {this.props.routes.map((route, i) => {
+                        return (
+                            <Text key = {i}>{route}</Text>
+                        );
+                    })}
+                </View>
+            </Card>
+        );
+    }
+}
