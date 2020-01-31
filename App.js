210dde8cd856d908572d56072bc97708bcbeb49c
Edward Balmer <edwardbalmer@gmail.com>
2020-01-31 20:19:26 +0000

    Logs pulled from backend

--- App.js
+++ App.js
@@ -5,29 +5,7 @@ import SessionLog from './components/sessionLog';
 
 export default function App() {
 
-
-  // fetch('http://192.168.1.97:3000/users', {method: "GET"})
-  // .then((response) => {
-  //   return response.json()
-  // })
-  // .then((myJson) => {
-  //   console.log(myJson)
-  // }
-  // )
-  // .catch((error) => {
-  //   console.log(error)}
-  // );
-
   return (
     <SessionLog />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/sessionLog.js
+++ components/sessionLog.js
@@ -1,31 +1,3 @@
-const sessions = [
-{
-    date: '21/01/20',
-    routes: ['V1', 'V1', 'V1', 'V1', 'V1', 'V1', 'V1', 'V1', 'V1']
-},
-{
-    date: '25/01/20',
-    routes: ['V1', 'V2', 'V2', 'V6', 'V5', 'V3', 'V2', 'V8', 'V1']
-}, 
-{
-    date: '31/01/20',
-    routes: ['V1']
-}, 
-{
-    date: '5/02/20',
-    routes: ['V8','V8']
-}, 
-{
-    date: '20/03/20',
-    routes: ['V1', 'V2','V1']
-}, 
-{
-    date: '28/04/20',
-    routes: ['V6', 'V5', 'V3', 'V2', 'V8', 'V1']
-}
-]
-
-
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
@@ -35,14 +7,35 @@ export default class SessionLog extends Component {
     constructor(props) {
         super(props);
 
-      }
+        this.state ={
+            sessions: []
+        }
+    }
+
+    getSessionLogs() {
+        fetch('http://192.168.1.97:3000/users', { method: "GET" })
+            .then((response) => {
+                return response.json()
+            }).then((session) => {
+                this.setState({sessions: session})
+            })
+            .catch((error) => {
+                console.log(error)
+            }
+            );
+    }
+
+    componentDidMount() {
+        this.getSessionLogs()
+    }
 
     render() {
+        
         return (
             <ScrollView>
-                {sessions.map((session, i)=> {
+                {this.state.sessions.map((session, i) => {
                     return (
-                        <SessionCard {...session} key = {i}/>
+                        <SessionCard {...session} key={i} />
                     );
                 })}
             </ScrollView>
